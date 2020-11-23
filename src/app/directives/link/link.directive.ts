25428ef4dbce33419643b42837e2cc45755dad4f
Bibek Saini <itsbibeksaini@gmail.com>
2020-11-23 20:26:30 +0530

    Link working

--- src/app/directives/link/link.directive.ts
+++ src/app/directives/link/link.directive.ts
@@ -1,14 +1,22 @@
-import { Directive, HostListener } from '@angular/core';
+import { Directive, HostListener, Input } from '@angular/core';
 import * as URLS from 'src/app/constants/urllinks';
 @Directive({
   selector: '[link]'
 })
 export class LinkDirective {
   
+  @Input() link!: string
   constructor() { }
 
   @HostListener("click") click(){
-    window.open(URLS.GITHUB)
+    if(this.link === 'github')
+      window.open(URLS.GITHUB)
+    else if(this.link === 'linkdin')
+      window.open(URLS.LINKEDIN)
+    else if(this.link === 'facebook')
+      window.open(URLS.FACEBOOK)
+    else if(this.link === 'instagram')
+      window.open(URLS.INSTAGRM)
   }
 
 }
