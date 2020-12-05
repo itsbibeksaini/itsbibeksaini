fdbd3568d5c7ec5c553b57cfd937d50f48be0f6b
Bibek Saini <itsbibeksaini@gmail.com>
2020-12-05 22:33:18 +0530

    Work experience template

--- src/app/components/work/work.component.ts
+++ src/app/components/work/work.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {WORK_EXPERIENCE} from 'src/app/data/work_experience';
 
 @Component({
   selector: 'app-work',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WorkComponent implements OnInit {
 
-  constructor() { }
+  workExperience = WORK_EXPERIENCE
+  constructor() {     
+  }
 
   ngOnInit(): void {
   }
