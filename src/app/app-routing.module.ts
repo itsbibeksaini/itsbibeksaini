5a3eba80c9c69cdb494a7c876fa463cd542b0af8
Bibek Saini <itsbibeksaini@gmail.com>
2021-04-02 17:16:13 +0530

    Sidebar update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { WorkComponent } from './components/work/work.component';
 
 const routes: Routes = [{
   path:'',
   component: HomeComponent
+},{
+  path:'work',
+  component: WorkComponent
+},{
+  path: 'projects',
+  component: ProjectsComponent
+},{
+  path: 'skills',
+  component: SkillsComponent
 }];
 
 @NgModule({
