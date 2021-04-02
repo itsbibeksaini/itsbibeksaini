a4dcca9f4c7990cb6e26ad29255352ff04669ce5
Bibek Saini <itsbibeksaini@gmail.com>
2021-04-02 15:27:05 +0530

    Sidebar update

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faCogs, faLaptopCode, faLaptopHouse } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'sidebar',
@@ -7,6 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
+  faLaptopHouse = faLaptopHouse
+  faLaptopCode = faLaptopCode
+  faCogs = faCogs
   constructor() { }
 
   ngOnInit(): void {
