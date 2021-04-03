b6b9a1e7cbf3bf0f70bfeb56381a0fc026c8036f
Bibek Saini <itsbibeksaini@gmail.com>
2021-04-03 12:54:15 +0530

    Shared module and custom list component

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CustomListComponent } from './custom-list/custom-list.component';
+
+
+
+@NgModule({
+  declarations: [CustomListComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
