b86e407e70367f490918abb2f14e3f447981d9bc
Bibek Saini <itsbibeksaini@gmail.com>
2021-04-04 09:32:50 +0530

    Update

--- src/app/data/work_experience.ts
+++ src/app/data/work_experience.ts
@@ -12,7 +12,7 @@ WORK_EXPERIENCE.push(seniorSoftwareEngineer)
 let softwareEngineer:Experience = new Experience()
 softwareEngineer.START_DATE = "May 2018"
 softwareEngineer.END_DATE = "December 2020"
-softwareEngineer.POSITION = "Software engineer."
+softwareEngineer.POSITION = "Software Developer."
 softwareEngineer.COMPANY = "CatalystOne"
 softwareEngineer.addJobDescription("Collaborated with cross-functional development team members to analyze potential system solutions based on evolving client requirements.")
 softwareEngineer.addJobDescription("Identified and tracked defects with SonarQube and supported developers in resolving problems by completing additional tests.")
@@ -24,7 +24,7 @@ WORK_EXPERIENCE.push(softwareEngineer);
 let associateSoftwareEngineer:Experience = new Experience()
 associateSoftwareEngineer.START_DATE = "July 2016"
 associateSoftwareEngineer.END_DATE = "May 2018"
-associateSoftwareEngineer.POSITION = "Associate software engineer."
+associateSoftwareEngineer.POSITION = "Associate Java Developer."
 associateSoftwareEngineer.COMPANY = "CatalystOne"
 associateSoftwareEngineer.addJobDescription("Researched, designed and implemented scalable applications for information identification, extraction, analysis, retrieval and indexing.")
 associateSoftwareEngineer.addJobDescription("Tested troubleshooting methods, devised innovative solutions, and documented resolutions for inclusion in knowledge base for support team use.")
@@ -37,7 +37,7 @@ WORK_EXPERIENCE.push(associateSoftwareEngineer);
 let trainee:Experience = new Experience()
 trainee.START_DATE = "April 2016"
 trainee.END_DATE = "July 2016"
-trainee.POSITION = "Trainee."
+trainee.POSITION = "Trainee Java Developer."
 trainee.COMPANY = "CatalystOne"
 trainee.addJobDescription("Participated with preparation of design documents for trackwork, including alignments, specifications, criteria details and estimates.")
 trainee.addJobDescription("Collaborated with developers and product owners to stay current on product features and intended functionality.")
