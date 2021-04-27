c374232b7e96a912e15e19efdfcf661249b45dac
Bibek Saini <itsbibeksaini@gmail.com>
2021-04-27 23:42:56 +0530

    adding project structure link

--- src/app/data/projects/mailer-details.ts
+++ src/app/data/projects/mailer-details.ts
@@ -8,7 +8,7 @@ mailer.TITLE = "Mailer",
 mailer.SUB_TITLE = "Angular app"
 mailer.DESCRIPTION = "Mailer app is built on angular framewrok providing mail viewing experience. The project is built as SPA where content is refreshed and their state is maintained. App has state management implemented using datorama/akita."
 mailer.APP_LINK = "http://mailer.itsbibeksaini.com"
-mailer.PROJECT_STRUCTURE_LINK = ""
+mailer.PROJECT_STRUCTURE_LINK = "https://mailer.itsbibeksaini.com/assets/documentation/overview.html"
 
 mailer.TECH_USED.push(frontendSkills.skill_angular)
 mailer.TECH_USED.push(frontendSkills.skill_akita)
