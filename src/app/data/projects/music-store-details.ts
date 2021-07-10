9fae2d3e103a9e81d6a1805bef45fc5652d7b246
Bibek Saini <itsbibeksaini@gmail.com>
2021-07-10 23:54:02 +0530

    Adding new project music store

---
+++ src/app/data/projects/music-store-details.ts
@@ -0,0 +1,8 @@
+import { Project } from "src/app/components/projects/models/project";
+
+export const musicstore: Project = new Project();
+musicstore.ID = "music_store"
+musicstore.TITLE = "Mailer",
+musicstore.SUB_TITLE = "ASP.NET MVC"
+musicstore.DESCRIPTION = ""
+musicstore.ARCHIEVED = true
--- src/app/data/projects/projects.ts
+++ src/app/data/projects/projects.ts
@@ -1,8 +1,10 @@
 import { Project } from "src/app/components/projects/models/project";
 import { mailer } from "./mailer-details";
+import { musicstore } from "./music-store-details";
 import { taskManager } from "./task-manager-details";
 
 export const PROJECTS_DETAILS:Project[] = []
 
 PROJECTS_DETAILS.push(taskManager)
 PROJECTS_DETAILS.push(mailer)
+PROJECTS_DETAILS.push(musicstore)
