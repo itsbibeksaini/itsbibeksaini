8cb162cf9a4143d533bcfcd7bdcbe6856ca64947
Bibek Saini <itsbibeksaini@gmail.com>
2021-07-18 18:52:09 +0530

    adding gallery images for music store project

--- src/app/data/projects/music-store-details.ts
+++ src/app/data/projects/music-store-details.ts
@@ -1,3 +1,4 @@
+import { GalleryImage } from "src/app/components/projects/models/gallery-image";
 import { Project } from "src/app/components/projects/models/project";
 
 export const musicstore: Project = new Project();
@@ -6,3 +7,12 @@ musicstore.TITLE = "Music Store",
 musicstore.SUB_TITLE = "ASP.NET MVC"
 musicstore.DESCRIPTION = ""
 musicstore.ARCHIEVED = true
+
+addGalelryImage("assets/projects/musicstore/musicstore_home1.png")
+addGalelryImage("assets/projects/musicstore/musicstore_home2.png")
+
+function addGalelryImage(imgSrc:string){
+    let galleryImage:GalleryImage = new GalleryImage()
+    galleryImage.IMAGE = imgSrc    
+    musicstore.GALLERY.push(galleryImage)
+}
