8a3f0eabc0d6006a674cfa2b957ea056ec0b4a21
Bibek Saini <itsbibeksaini@gmail.com>
2021-08-01 23:54:09 +0530

    Image preview dialog improvements

--- src/app/components/gallery/image-viewer/image-viewer.component.ts
+++ src/app/components/gallery/image-viewer/image-viewer.component.ts
@@ -1,5 +1,6 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-image-viewer',
@@ -7,6 +8,8 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
   styleUrls: ['./image-viewer.component.scss']
 })
 export class ImageViewerComponent implements OnInit {
+
+  faTimes = faTimes
 
   prevImages:string[];
   constructor(@Inject(MAT_DIALOG_DATA) public data: string[]) { 
